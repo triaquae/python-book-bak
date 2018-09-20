0278e2d47963b023b23fe3a859ca6385117121c8
AgoCan <578843228@qq.com>
2018-09-20 08:55:14 +0000

    Updates baidutongji/baidu.js
    Auto commit by GitBook Editor

---
+++ baidutongji/baidu.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?9cae5942a3c39f3b6fcf0a32b00277e2";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
